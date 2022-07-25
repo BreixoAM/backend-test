731694cb74d859ce797cdf9c4faf17c58dac67b6
Breixo Alvarez Martinez <breixoam@gmail.com>
2022-07-25 09:39:12 +0200

    Removed not needed catch

--- src/Beer/Infrastructure/Client/PunkApiClient.php
+++ src/Beer/Infrastructure/Client/PunkApiClient.php
@@ -8,7 +8,6 @@ use App\Beer\Domain\Entity\Beer;
 use App\Beer\Domain\Exception\NotFoundException;
 use App\Beer\Domain\Factory\BeerFactory;
 use App\Beer\Domain\Repository\BeerRepositoryInterface;
-use Exception;
 use loophp\collection\Collection;
 use loophp\collection\Contract\Collection as CollectionInstance;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
@@ -33,7 +32,7 @@ class PunkApiClient implements BeerRepositoryInterface
         try {
             $response = $this->client->request('GET', $url);
             $content = $response->toArray()[0];
-        } catch (Exception|Throwable $e) {
+        } catch (Throwable $e) {
             throw new NotFoundException($e->getMessage());
         }
 
